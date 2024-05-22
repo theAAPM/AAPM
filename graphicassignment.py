0c700cbff6a028951c3eba98d25582aab8ccb860
theAAPM <1257306322@qq.com>
2024-05-22 17:05:53 +0800

    Add files via upload

---
+++ graphicassignment.py
@@ -0,0 +1,106 @@
+import cv2
+import subprocess
+import random
+import os
+import numpy as np
+
+def get_random_image_size(video_path):
+    video = cv2.VideoCapture(video_path)
+    _, frame = video.read()
+    video_width = frame.shape[1]
+    video_height = frame.shape[0]
+    video.release()
+
+    max_image_width = int(video_width / 2.5)
+    max_image_height = int(video_height / 2.5)
+    image_width = random.randint(int(max_image_width / 2.5), max_image_width)
+    image_height = random.randint(int(max_image_height / 2.5), max_image_height)
+    
+    return (image_width, image_height)
+
+def get_random_position(video_path, image_size):
+    # 获取视频大小
+    video = cv2.VideoCapture(video_path)
+    _, frame = video.read()
+    video_width = frame.shape[1]
+    video_height = frame.shape[0]
+    video.release()
+
+    max_x = video_width - image_size[0]
+    max_y = video_height - image_size[1]
+
+    x = random.randint(0, max_x)
+    y = random.randint(0, max_y)
+    
+    return (x, y)
+
+def resize_image(image_path, output_path, target_size):
+
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    resized_image = cv2.resize(image, target_size)
+    
+    # random_angle = np.random.randint(0, 90)
+    # (h, w) = image.shape[:2]
+    # center = (w // 2, h // 2)
+    # rotation_matrix = cv2.getRotationMatrix2D(center, random_angle, 1.0)
+
+
+    # cosine = np.abs(rotation_matrix[0, 0])
+    # sine = np.abs(rotation_matrix[0, 1])
+    # new_w = int((h * sine) + (w * cosine))
+    # new_h = int((h * cosine) + (w * sine))
+
+    # rotated_image = cv2.warpAffine(resized_image, rotation_matrix, (new_w, new_h))
+    
+
+    # flip_code = random.randint(-1, 1) 
+    # flipped_image = cv2.flip(rotated_image, flip_code)
+
+    cv2.imwrite(output_path, resized_image)
+    
+
+def insert_image_in_video(video_path, image_path, output_path, start_time):
+    target_size = get_random_image_size(video_path)
+    resized_image_path = "resized_image.png"
+    resize_image(image_path, resized_image_path, target_size)
+    
+    position = get_random_position(video_path, target_size)
+    position_str = f"{position[0]}:{position[1]}"
+    
+    # 使用FFmpeg命令将调整后的图片插入视频
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    command = f'ffmpeg -i {video_path} -i {resized_image_path} -filter_complex "overlay={position_str}" -c:a copy -y -ss {start_time} {output_path}'
+    
+    subprocess.call(command, shell=True)
+    
+    os.remove(resized_image_path)
+
+
+
+start_time = '00:00:00' 
+
+video_file = open('/mnt/data/xiao/code/auto/boost/train_few_shot.txt', 'r')
+video_paths = video_file.readlines()
+random.shuffle(video_paths)
+video_file.close()
+
+
+image_folder = '/mnt/data/xiao/code/auto/boost/pic/低级形状'  # 指定图片所在的文件夹路径
+image_files = [filename for filename in os.listdir(image_folder) if filename.endswith(('.png', '.jpg', '.jpeg'))]
+
+
+for path in video_paths:
+    video_path = path.strip().split('//') [-1]
+    video_path = os.path.join('/mnt/data/k400few', video_path)
+    video_name = path.strip().split('/') [-1]
+    random_image = random.choice(image_files)
+    image_path = os.path.join(image_folder, random_image)
+    folder = random_image.split('.')[0]
+    destination = os.path.join('/mnt/data/k400few/low', folder, video_name)
+    insert_image_in_video(video_path, image_path, destination, start_time)
+
+print('finished')
+
+
+
+    
